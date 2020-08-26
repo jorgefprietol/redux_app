5c65218536c7d21a896882de68884627ac19a6d0
Jorge Fidel Prieto Linares <jorgefprietol@gmail.com>
2020-08-26 15:51:31 -0500

    Conexion de los botones con las acciones

--- src/component/botonera/Botonera.js
+++ src/component/botonera/Botonera.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './Botonera.css';
 
+import navReducer from '../../redux/reducers/navReducer';
+import nav_click from '../../redux/actions/navActions';
+import { connect } from 'react-redux';
+
+
+
 const Botonera = () => {
+
+  const buttonClick = (title) => {
+    nav_click({
+      title: title,
+    });
+  }
+
   return (
     <div className="Botonera">
       <header className="App-header">
@@ -11,13 +24,22 @@ const Botonera = () => {
             </h1>
         </div>
         <ul>
-            <li>Home</li>
-            <li>List</li>
-            <li>New</li>
+            <li onClick={() =>buttonClick('Home')}>Home</li>
+            <li onClick={() =>buttonClick('List')}>List</li>
+            <li onClick={() =>buttonClick('New')}>New</li>
         </ul>
       </header>
     </div>
   );
 }
 
-export default Botonera;
+const mapStateToProps = (state) => {
+  return {
+    title: state.navState.title,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { nav_click }
+)(Botonera);

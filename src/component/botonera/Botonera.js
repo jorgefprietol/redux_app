8456c509478c39a1a6b5a2c074e21e31ad9e9c26
Jorge Fidel Prieto Linares <jorgefprietol@gmail.com>
2020-08-26 15:28:02 -0500

    Vista Inicial de la app

---
+++ src/component/botonera/Botonera.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Botonera.css';
+
+const Botonera = () => {
+  return (
+    <div className="Botonera">
+      <header className="App-header">
+        <div>
+            <h1>
+                Titulo
+            </h1>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>List</li>
+            <li>New</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Botonera;

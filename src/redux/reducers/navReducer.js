b4b26cb31d4390cd1185d6be7ac312356f34328a
Jorge Fidel Prieto Linares <jorgefprietol@gmail.com>
2020-08-26 15:36:10 -0500

    Programacion de un reducer simple

--- src/redux/reducers/navReducer.js
+++ src/redux/reducers/navReducer.js
@@ -1,7 +1,20 @@
 import * as types from '../actions/actionTypes';
+import { register } from '../../serviceWorker';
 
-const navState = (state, action) => {
+const initialState = {
+    title: 'Home',
+}
 
+const navState = (state = initialState, action) => {
+    switch (action.type) {
+        case types.NAV_CLICK: {
+            return {
+                ...state,
+                title: action.payload.title,
+            }
+        }
+        default: return state;
+    }
 }
 
 export default navState;
